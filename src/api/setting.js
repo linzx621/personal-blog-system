02dea19594d14e441368102245b692c73571f8c0
linzx621 <2846282482@qq.com>
2021-05-14 22:22:57 +0800

    更新2ing

--- src/api/setting.js
+++
@@ -1,8 +0,0 @@
-import request from './request';
-import "@/mock/setting";
-
-
-export default async function getSetting() {
-    const resp = await request.get('/api/setting');
-    return resp;
-}
--- src/utils/debounce.js
+++
@@ -1,9 +0,0 @@
-export default function(func, duration = 50) {
-    let timer = null;
-    return function(...args) {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            func(...args);
-        }, duration)
-    }
-}
